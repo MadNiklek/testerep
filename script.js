bb0a4c91e85a7c3a58d28612bf3135137163a2b0
MadNiklek <madniklek@gmail.com>
2019-08-12 19:23:10 +0300

    added nav and prog bars

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// When the user scrolls the page, execute myFunction 
+// STICK SECOND NAV
 window.onscroll = function() {myFunction()};
-
 // Get the navbar
 var navbar = document.getElementById("nav2");
-
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
-
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
@@ -15,3 +12,21 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+
+//PROGRESS BAR
+document.addEventListener(
+  "scroll",
+  function() {
+    var scrollTop =
+      document.documentElement["scrollTop"] || document.body["scrollTop"];
+    var scrollBottom =
+      (document.documentElement["scrollHeight"] ||
+        document.body["scrollHeight"]) - document.documentElement.clientHeight;
+    scrollPercent = scrollTop / scrollBottom * 100 + "%";
+    document
+      .getElementById("_progress")
+      .style.setProperty("--scroll", scrollPercent);
+  },
+  { passive: true }
+);
