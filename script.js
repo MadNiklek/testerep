e28dd0855f76c6b17e3b8a6f44ef7f7e23cca5af
MadNiklek <madniklek@gmail.com>
2019-08-12 19:34:10 +0300

    Fixed progress bar issue and placed beneath nav2

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ window.onscroll = function() {myFunction()};
 var navbar = document.getElementById("nav2");
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
+// progress scroll
+var progscr = document.getElementById("_progress");
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
+    document.getElementById('_progress').style.display = 'block';
   } else {
     navbar.classList.remove("sticky");
+    document.getElementById('_progress').style.display = 'none';
   }
 }
 
